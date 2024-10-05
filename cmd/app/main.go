387b62d2e214dbb2e9a7c9d3425fe6d39e4e0d0a
Kheyssper <gaspar.cossa@uzambeze.ac.mz>
2024-10-05 19:30:05 +0200

    Server and the main function

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"log"
+	"os"
+	"saasmicroservice/pkg/db"
+	"saasmicroservice/pkg/router"
+	"saasmicroservice/pkg/server"
+)
+
 func main() {
-	println("dasda")
+	// Conectar ao banco de dados
+	dbConn, err := db.NewPostgresDB()
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	// Configurar o roteador
+	r := router.SetupRouter(dbConn)
+
+	// Iniciar o servidor
+	srv := server.Server{}
+	port := os.Getenv("SERVER_PORT")
+	if port == "" {
+		port = "8080" // Porta padrão
+	}
+	if err := srv.Run(port, r); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 }
